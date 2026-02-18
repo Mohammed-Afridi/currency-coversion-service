26809a7649e9403733a69fd8070c77c746e1d653
mohammed_git <mohammedafridi62@gmail.com>
2026-02-19 01:50:21 +0530

    invoke currency exchange api using rest template

--- src/main/java/com/microservices/currency_conversion_service/CurrencyConversionController.java
+++ src/main/java/com/microservices/currency_conversion_service/CurrencyConversionController.java
@@ -1,26 +1,35 @@
 package com.microservices.currency_conversion_service;
 
+import org.jspecify.annotations.Nullable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
 
 
     @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
-    public CurrencyConversion calculateCurrencyConversion(
-            @PathVariable String from,
-            @PathVariable String to,
-            @PathVariable BigDecimal quantity
-    ){
+    public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity){
 
-        return new CurrencyConversion(1001L,
-                from,to,quantity,
-                BigDecimal.ONE,
-                BigDecimal.ONE,
-                "");
+        // Rest templat is used to make rest api calls
+        //getForEntity -> we want to send a get request
+        // Currency.class -> the return response should be this structure
+        // urivariables -> pass values from and to
+        HashMap<String,String> uriVariables = new HashMap<>();
+        uriVariables.put("from",from);
+        uriVariables.put("to",to);
+
+        ResponseEntity<CurrencyConversion> responseEntity =
+                new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, uriVariables);
+
+        CurrencyConversion currencyConversion = responseEntity.getBody();
+
+        return new CurrencyConversion(currencyConversion.getId(), from,to,quantity, currencyConversion.getConversionMultiple(), quantity.multiply(currencyConversion.getConversionMultiple()), currencyConversion.getEnvironment());
     }
 }
