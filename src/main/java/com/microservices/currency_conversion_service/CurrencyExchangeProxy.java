9ce08f944e0e36cda35087278c1b122c141983df
mohammed_git <mohammedafridi62@gmail.com>
2026-02-22 18:27:42 +0530

    feign changes

--- src/main/java/com/microservices/currency_conversion_service/CurrencyExchangeProxy.java
+++ src/main/java/com/microservices/currency_conversion_service/CurrencyExchangeProxy.java
@@ -4,7 +4,13 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="currency-exchange", url = "localhost:8000")
+//@FeignClient(name="currency-exchange", url = "localhost:8000")
+
+// We want the fiegn client talk to eureka , pick up the instances of CES and do load balancing between them
+// All that magic would happen by removing url=localhost
+// So now we dont have specific instance configured of CES
+
+@FeignClient(name="currency-exchange")
 public interface CurrencyExchangeProxy {
     // I need to call currency exchange service,
     // hence copy its currency exchage Controller class URI and method definition
